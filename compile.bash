6427e5db7f56141745c8284b8ad9d8dadfdb4e0c
Sean Cantwell <cantwellsean0127@gmail.com>
2025-03-03 09:20:42 -0600

    Create compiler

---
+++ compile.bash
@@ -0,0 +1,20 @@
+	#!/bin/bash
+
+# Find all subdirectories and loop through them
+for dir in *; do
+
+    # Check if the directory contains a src directory
+    if [[ -d ./$dir/src ]]; then
+
+        # Create a bin directory if it doesn't exist
+        mkdir -p ./$dir/bin
+
+        # Compile for Linux
+        g++ ./$dir/src/*.cpp -o ./$dir/bin/linux.exe
+
+        # Compile for Windows
+        x86_64-w64-mingw32-g++ ./$dir/src/*.cpp -o ./$dir/bin/windows.exe
+
+    fi
+done
+
