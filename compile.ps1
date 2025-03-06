21b3f7ca75e220ba40c23cceaed61203e0932700
Sean Cantwell <cantwellsean0127@gmail.com>
2025-03-05 16:45:02 -0800

    Update compiler script for Windows

--- compile.ps1
+++ compile.ps1
@@ -1,4 +1,8 @@
 $problem_directory = Get-Location
 $src_directory = "$problem_directory/src"
 $bin_directory = "$problem_directory/bin"
+if (!(Test-Path -Path "$bin_directory"))
+{
+    New-Item -ItemType "directory" -Path "$bin_directory"
+}
 g++ "$src_directory/*" -o "$bin_directory/windows.exe"  
